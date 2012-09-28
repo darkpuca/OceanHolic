01b215be65241ff8e2ad001b5c01c67185c06cd9
DongKyu Kim <darkpuca@gmail.com>
2012-09-28 21:33:20 +0900

    Initial Commit

---
+++ OceanHolic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OceanHolic
+//
+//  Created by darkpuca on 9/28/12.
+//  Copyright (c) 2012 darkpuca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ OceanHolic/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  OceanHolic
+//
+//  Created by darkpuca on 9/28/12.
+//  Copyright (c) 2012 darkpuca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
